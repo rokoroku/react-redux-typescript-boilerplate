a74b652df3877805c49d90b6fb1e9394241f3b25
Young Rok Kim <rok0810@gmail.com>
2017-04-28 10:28:38 +0900

    Fix type error of @connect from react-redux (fixes #11)
    Update typescript to 2.3.1

--- types/react-redux.d.ts
+++ types/react-redux.d.ts
@@ -4,11 +4,10 @@ declare module 'react-redux' {
 
   // overload connect interface to return built-in ClassDecorator
   // https://github.com/reactjs/react-redux/pull/541#issuecomment-269197189
-
-  export function connect(
-    mapStateToProps?: MapStateToProps<any, any> | MapStateToPropsFactory<any, any>,
-    mapDispatchToProps?: MapDispatchToProps<any, any> | MapDispatchToPropsFactory<any, any>,
-    mergeProps?: MergeProps<any, any, any>,
+  export function connect<TStateProps = any, TDispatchProps = any, TOwnProps = any, TMergedProps = any>(
+    mapStateToProps: MapStateToPropsParam<TStateProps, TOwnProps>,
+    mapDispatchToProps?: MapDispatchToPropsParam<TDispatchProps, TOwnProps>,
+    mergeProps?: MergeProps<TStateProps, TDispatchProps, TOwnProps, TMergedProps>,
     options?: Options
-  ): ClassDecorator;
+  ): InferableComponentDecorator<TOwnProps>;
 }
