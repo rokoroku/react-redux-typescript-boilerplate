afe26233d5ddf0622a31ec48be606fe4316e772b
Young Rok Kim <rok0810@gmail.com>
2017-06-10 19:14:35 +0900

    Update dependencies

--- src/actions/todos.ts
+++ src/actions/todos.ts
@@ -5,5 +5,5 @@ export const addTodo = createAction<TodoItemData>(Actions.ADD_TODO);
 export const editTodo = createAction<TodoItemData>(Actions.EDIT_TODO);
 export const deleteTodo = createAction<TodoItemId>(Actions.DELETE_TODO);
 export const completeTodo = createAction<TodoItemId>(Actions.COMPLETE_TODO);
-export const completeAll = createAction<void>(Actions.COMPLETE_ALL);
-export const clearCompleted = createAction<void>(Actions.CLEAR_COMPLETED);
+export const completeAll = createAction(Actions.COMPLETE_ALL);
+export const clearCompleted = createAction(Actions.CLEAR_COMPLETED);
