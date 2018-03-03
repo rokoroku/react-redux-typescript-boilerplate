094e74981304ed147f56ddccf39bbdba53502e05
Youngrok Kim <rok0810@gmail.com>
2018-03-04 03:58:20 +0900

    Add GitHub page

--- src/app/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { Footer } from './Footer';
-export { Header } from './Header';
-export { TodoList } from './TodoList';
-export { TodoItem } from './TodoItem';
-export { TodoTextInput } from './TodoTextInput';
--- src/app/middleware/logger.ts
+++
@@ -1,6 +0,0 @@
-import { Middleware } from 'redux';
-
-export const logger: Middleware = (store) => (next) => (action) => {
-  console.log(action);
-  return next(action);
-};
