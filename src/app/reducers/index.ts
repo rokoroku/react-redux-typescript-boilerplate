98754252a20c3335fa3feb8f28e46ef4eb45d78a
Alireza <alireza.mh@gmail.com>
2019-07-09 14:00:49 +0430

    Update packages and npm audit

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,6 +6,6 @@ export { RootState };
 
 // NOTE: current type definition of Reducer in 'redux-actions' module
 // doesn't go well with redux@4
-export const rootReducer = combineReducers<RootState>({
+export const rootReducer = combineReducers({
   todos: todoReducer as any
 });
