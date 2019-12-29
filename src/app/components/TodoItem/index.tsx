bb04043b9b04235e1f3cabfcd33853677da27aa7
William Tio <wtoa.code@gmail.com>
2019-12-29 13:03:38 +1100

    Refactor TodoItem to React hooks

--- src/app/components/TodoItem/index.tsx
+++ src/app/components/TodoItem/index.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import * as classNames from 'classnames';
-import * as style from './style.css';
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import style from './style.css';
 import { TodoModel } from 'app/models';
 import { TodoActions } from 'app/actions';
 import { TodoTextInput } from '../TodoTextInput';
@@ -18,37 +18,27 @@ export namespace TodoItem {
   }
 }
 
-export class TodoItem extends React.Component<TodoItem.Props, TodoItem.State> {
-  constructor(props: TodoItem.Props, context?: any) {
-    super(props, context);
-    this.state = { editing: false };
-  }
+export const TodoItem = ({ todo, editTodo, deleteTodo, completeTodo }: TodoItem.Props) => {
+  const [editing, setEditing] = useState(false);
 
-  handleDoubleClick() {
-    this.setState({ editing: true });
-  }
+  const handleDoubleClick = () => {
+    setEditing(true);
+  };
 
-  handleSave(id: number, text: string) {
+  const handleSave = (id: number, text: string) => {
     if (text.length === 0) {
-      this.props.deleteTodo(id);
+      deleteTodo(id);
     } else {
-      this.props.editTodo({ id, text });
+      editTodo({ id, text });
     }
-    this.setState({ editing: false });
-  }
+    setEditing(false);
+  };
 
-  render() {
-    const { todo, completeTodo, deleteTodo } = this.props;
-
-    let element;
-    if (this.state.editing) {
-      element = (
-        <TodoTextInput
-          onSave={(text) => todo.id && this.handleSave(todo.id, text)}
-        />
-      );
+  const renderElement = (): JSX.Element => {
+    if (editing) {
+      return <TodoTextInput onSave={(text) => todo.id && handleSave(todo.id, text)} />;
     } else {
-      element = (
+      return (
         <div className={style.view}>
           <input
             className={style.toggle}
@@ -56,7 +46,7 @@ export class TodoItem extends React.Component<TodoItem.Props, TodoItem.State> {
             checked={todo.completed}
             onChange={() => todo.id && completeTodo(todo.id)}
           />
-          <label onDoubleClick={() => this.handleDoubleClick()}>{todo.text}</label>
+          <label onDoubleClick={() => handleDoubleClick()}>{todo.text}</label>
           <button
             className={style.destroy}
             onClick={() => {
@@ -66,14 +56,13 @@ export class TodoItem extends React.Component<TodoItem.Props, TodoItem.State> {
         </div>
       );
     }
+  };
 
-    // TODO: compose
-    const classes = classNames({
-      [style.completed]: todo.completed,
-      [style.editing]: this.state.editing,
-      [style.normal]: !this.state.editing
-    });
+  const classes = classNames({
+    [style.completed]: todo.completed,
+    [style.editing]: editing,
+    [style.normal]: !editing
+  });
 
-    return <li className={classes}>{element}</li>;
-  }
-}
+  return <li className={classes}>{renderElement()}</li>;
+};
